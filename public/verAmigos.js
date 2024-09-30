7b468fb6eac66691451a128c0aeba975001cc6f8
richi3883893 <yerissalamanca197531@gmail.com>
2024-09-30 10:55:57 -0500

    datos act

--- public/verAmigos.js
+++ public/verAmigos.js
@@ -2,7 +2,9 @@ let traerAmigosBoton = document.getElementById("traerAmigos");
 let contentverAmigo = document.getElementById("verAmigo");
 let contentAmigo = document.getElementById("contentAmigo");
 
-
+fetch("/veramigos")
+.then(response => response.json())
+.then(data => console.log(data))
 
 traerAmigosBoton.addEventListener("click", () => {
     window.location.href = 'carga.html';
