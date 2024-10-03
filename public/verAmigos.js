f30701576b0185875f995616438fa09141f2bb9a
richi3883893 <yerissalamanca197531@gmail.com>
2024-10-03 08:06:21 -0500

    resp

--- public/verAmigos.js
+++ public/verAmigos.js
@@ -1,13 +1,16 @@
-let traerAmigosBoton = document.getElementById("traerAmigos");
+let traerAmigosBoton = document.getElementById("traerAmigos")
 let contentverAmigo = document.getElementById("verAmigo");
 let contentAmigo = document.getElementById("contentAmigo");
 
 fetch("/veramigos")
 .then(response => response.json())
-.then(data => data.data.forEach(element => {
-    console.log(element.amigo , element.numero)
-    console.log(" numero de amigos secretos" , data.data.length)
+.then(data => data.data.array.forEach(element => {
+    console.log(element.numero , element.amigo)
+    console.log("numero de amigos disponibles" , data.data.length)
 }))
+    
+   
+
 
 traerAmigosBoton.addEventListener("click", () => {
     window.location.href = 'carga.html';
